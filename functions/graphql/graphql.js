aff8315258a994d97804d372ed7e03a5e121e57d
Tim Millar <heteroskedasticity99@gmail.com>
2020-04-22 00:00:09 +0100

    back graphql with faunaDB

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,4 +1,8 @@
 const { ApolloServer, gql } = require("apollo-server-lambda");
+const faunadb = require("faunadb");
+const q = faunadb.query;
+
+const client = new faunadb.Client({ secret: process.env.FAUNA });
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -16,24 +20,61 @@ const typeDefs = gql`
   }
 `;
 
-const todos = {};
-let todoIndex = 0;
-
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    todos: (parent, args, { user }) => (user ? Object.values(todos) : []),
+    todos: async (parent, args, { user }) => {
+      if (!user) return [];
+      else {
+        const results = await client.query(
+          q.Paginate(q.Match(q.Index("todos_by_user"), user))
+        );
+        return results.data.map(([ref, text, done]) => ({
+          id: ref.id,
+          text,
+          done,
+        }));
+      }
+    },
   },
   Mutation: {
-    addTodo: (_, { text }) => {
-      todoIndex++;
-      const id = `key-${todoIndex}`;
-      todos[id] = { id, text, done: false };
-      return todos[id];
+    addTodo: async (_, { text }, { user }) => {
+      if (!user) {
+        throw new Error("Must be authenticated to create new todos");
+      }
+
+      const results = await client.query(
+        q.Create(q.Collection("todos"), {
+          data: {
+            text,
+            done: false,
+            owner: user,
+          },
+        })
+      );
+      return {
+        ...results.data,
+        id: results.ref.id,
+      };
     },
-    updateTodoDone: (_, { id }) => {
-      todos[id].done = !todo[id].done;
-      return todos[id];
+
+    updateTodoDone: async (_, { id }, { user }) => {
+      if (!user) {
+        throw new Error("Must be authenticated to create new todos");
+      }
+
+      const results = await client.query(
+        q.Update(q.Ref(q.Collection("todos"), id), {
+          data: {
+            done: true,
+          },
+        })
+      );
+
+      return {
+        ...results.data,
+        id: results.ref.id,
+      };
     },
   },
 };
